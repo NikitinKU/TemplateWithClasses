5811ede82871fc74d14d049a24f31c07bfe014dd
NikitinKU <90148254+NikitinKU@users.noreply.github.com>
2022-03-05 14:58:24 +0300

    Task1_var5 (almost done)
    У меня два вопроса:
    1) я не понимаю, как реализовать перегрузку вывода в моем случае (посмотрите код, пожалуйста). Как мне выводить то, что я вывожу в функции мейн на данном этапе?
    2) Нужно ли реализовывать вывод времени по типу: "08:02:58" (у меня сейчас в таком случае выведется "8:2:58") ?

--- CompletedTasks/Task1_var5/Task1/main.cpp
+++ CompletedTasks/Task1_var5/Task1/main.cpp
@@ -16,17 +16,18 @@
 
 #include <iostream>
 #include <string>
+#include <math.h>
 using namespace std;
 
 class Time {
 
 public:
 
-    int hour, min, sec;
+    int days, hour, min, sec;
 
     // Default constructor
     Time() {
-        hour = min = sec = 0;
+        days = hour = min = sec = 0;
     }
 
     // Destructor
@@ -34,6 +35,7 @@ public:
 
     // Copy constructor
     Time(const Time& time) {
+        days = time.days;
         hour = time.hour;
         min = time.min;
         sec = time.sec;
@@ -46,24 +48,13 @@ public:
 
     // Method of the interpreting time into the correct format 
     void TimeEdit() {
+        days += ((hour + (min + (sec / 60)) / 60) / 24);
         hour = (hour + ((min + (sec / 60)) / 60)) % 24;
         min = (min + (sec / 60)) % 60;
         sec %= 60;
     }
 
-    // Time addition method (+) 
-    void TimeAction(const Time& time) {
-        //finish
-        TimeEdit();
-    }
-
-    // Time subtraction method (-)
-    void TimeAction(const Time& time) {
-        //finish
-        TimeEdit();
-    }
-
-    void text(string userTime) {
+    void StringToInt(string userTime) {
         char strToInt[10] = { 0 };
         for (int i = 0, j = 0, tSection = 0; i <= userTime.length(); i++) {
             if (userTime[i] == ':' || userTime[i] == ' ' || i == userTime.length()) {
@@ -88,20 +79,116 @@ public:
             }
         }
     }
-    // Time comparison method (==)
 
+    // Time assignment overload (=)
+    Time& operator= (const Time& right) {
+        if (this == &right)
+            return *this;
+        hour = right.hour;
+        min = right.min;
+        sec = right.sec;
+        return *this;
+    }
+
+    // Time addition overload (+) 
+    Time operator+ (const Time& right) {
+        Time res;
+        res.hour = this -> hour + right.hour;
+        res.min = this-> min + right.min;
+        res.sec = this-> sec + right.sec;
+        return res;
+    }
+
+    // Time subtraction overload (-)
+    Time operator- (const Time& right) {
+        Time res;
+        res.hour = this->hour - right.hour;
+        res.min = this->min - right.min;
+        res.sec = this->sec - right.sec;
+        return res;
+    }
+
+    // Time comparison overload (==)
+    bool operator== (const Time& right) {
+       return ((this->hour == right.hour) && (this->min == right.min) && (this->sec == right.sec));
+    }
+
+    //friend ostream& operator<<(ostream& out, const Time& right);
 };
+
+//ostream& operator<<(ostream& out, const Time& right) {                // !!! help me here, please !!!
+//    out << "\nThe total time is: " << totalTime.hour << ":"
+//                                   << totalTime.min << ":"
+//                                   << totalTime.sec;
+//};
 
 int main() {
     srand((unsigned)time(NULL));
+    int menuChooser = 0;
     string userTime;
+    Time firstTime, secondTime, totalTime;
 
-    cout << "Enter the time (Format: hours:minutes:seconds) : ";
-
-    Time firstTime;
-
+    cout << "Enter the time" << "Format : hours ' ' / : minutes ' ' / : seconds : ";
     getline(cin, userTime);
-    firstTime.text(userTime);
+    firstTime.StringToInt(userTime);
+
+    cout << "\nEnter the time you want to: add (+), subtract (-), compare (==)" << "\nFormat : hours ' ' / : minutes ' ' / : seconds" << endl;
+    cout << "Your input is : ";
+    getline(cin, userTime);
+    secondTime.StringToInt(userTime);
+
+    int isWork = 1;
+
+    while (isWork) {
+        cout << "\nInput the number of the action" 
+            << "\n\t1) input 1 to add (+)" 
+            << "\n\t2) input 2 to subtract (-)" 
+            << "\n\t3) input 3 to compare (==)" 
+            << "\n\t4) input 0 to exit" << endl;
+        cout << "Your input is : ";
+        cin >> menuChooser;
+        switch (menuChooser) {
+        case 0:
+            isWork = 0;
+            break;
+
+        case 1:
+            totalTime.hour = firstTime.hour + secondTime.hour;
+            totalTime.min = firstTime.min + secondTime.min;
+            totalTime.sec = firstTime.sec + secondTime.sec;
+            totalTime.TimeEdit();
+            cout << "\nThe total time is: " << totalTime.days << ":"
+                                            << totalTime.hour << ":"
+                                            << totalTime.min << ":"
+                                            << totalTime.sec << endl;
+            break;
+
+        case 2:
+            totalTime.hour = abs(firstTime.hour - secondTime.hour);
+            totalTime.min = abs(firstTime.min - secondTime.min);
+            totalTime.sec = abs(firstTime.sec - secondTime.sec);
+            totalTime.TimeEdit();
+            cout << "\nThe total time is: " << totalTime.days << ":"
+                                            << totalTime.hour << ":"
+                                            << totalTime.min << ":" 
+                                            << totalTime.sec << endl;
+            break;
+
+        case 3:
+            bool result;
+            if (bool result = (firstTime.hour == secondTime.hour) && 
+                (firstTime.min == secondTime.min) && (firstTime.sec == secondTime.sec))
+                cout << "\nThe times are equal to each other" << endl;
+            else cout << "\nThe times aren't equal to each other" << endl;
+
+            break;
+
+        default:
+            cout << "\nERROR! You've entered the wrong operation!" << endl;
+
+            break;
+        }
+    }
 
     return 0;
 }
